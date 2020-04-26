c4cd3e762a294374477c604f189a970080833535
Ivan Morett <luisivanmorett@gmail.com>
2020-04-26 15:26:03 -0500

    user can modify and delete items

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './resources/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
+import { Toast } from 'react-bootstrap';
 import Laboratorio from './pages/Laboratorio';
 import Home from './pages/Home';
 import Contacto from './pages/Contacto';
@@ -15,11 +16,12 @@ import SignInModal from './components/SignInModal';
 
 function App() {
 
-    const [ signInShow , setSignInShow ] = useState(false);
-    const [ loggedIn , setLoggedIn ] = useState(localStorage.getItem("token")!=null);
-    const handleSignInClose = () => {setSignInShow(false);}
+    const [signInShow, setSignInShow] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(localStorage.getItem("token") != null);
+    const [notifications, setNotifications] = useState<Array<JSX.Element>>([]);
+    const handleSignInClose = () => { setSignInShow(false); }
     const handleLoginModalLogout = () => {
-        if(loggedIn){
+        if (loggedIn) {
             localStorage.clear();
             setLoggedIn(false);
         }
@@ -28,9 +30,29 @@ function App() {
         }
     }
 
+    const showNotification = (message: string) => {
+        let copynotfs = [...notifications];
+        const key = Date.now();
+        copynotfs.push(
+            <Toast key={key} onClose={removeNotification(key)} delay={2500} autohide>
+                <Toast.Header>
+                    <strong className="mr-auto">Sistema de Inventario</strong>
+                </Toast.Header>
+                <Toast.Body>{message}</Toast.Body>
+            </Toast>
+        )
+        setNotifications(copynotfs);
+    }
+
+    const removeNotification = (key: any) => {
+        return () => {
+            setNotifications(notifications.filter((item) => (item.key !== key)));
+        }
+    }
+
     return (
         <div className="App h-100">
-            <MainBar signInLogOut={handleLoginModalLogout} loggedIn={loggedIn}/>
+            <MainBar signInLogOut={handleLoginModalLogout} loggedIn={loggedIn} />
             <Switch>
                 <Route path="/" exact={true} component={Home} />
                 <Route path="/laboratorio" exact={true} component={Laboratorio} />
@@ -42,16 +64,26 @@ function App() {
                 <Route path="/materiales/equipo" exact={true} render={(props) => <ItemList {...props} type="equipo" />} />
                 <Route path="/materiales/proveedores" exact={true} render={(props) => <ItemList {...props} type="proveedores" />} />
                 {/* <Route path="/materiales/reporte" exact={true} render={(props) => <ItemList {...props} type="reporte" />} /> */}
-                <Route path="/materiales/material/:id" children={<Item type="material" />} />
-                <Route path="/materiales/consumibles/:id" children={<Item type="consumibles" />} />
-                <Route path="/materiales/reactivo/:id" children={<Item type="reactivos" />} />
-                <Route path="/materiales/equipo/:id" children={<Item type="equipo" />} />
-                <Route path="/materiales/proveedores/:id" children={<Item type="proveedores" />} />
+                <Route path="/materiales/material/:id" children={<Item type="material" loggedIn={loggedIn} showNotification={showNotification} />} />
+                <Route path="/materiales/consumibles/:id" children={<Item type="consumibles" loggedIn={loggedIn} showNotification={showNotification} />} />
+                <Route path="/materiales/reactivo/:id" children={<Item type="reactivos" loggedIn={loggedIn} showNotification={showNotification} />} />
+                <Route path="/materiales/equipo/:id" children={<Item type="equipo" loggedIn={loggedIn} showNotification={showNotification} />} />
+                <Route path="/materiales/proveedores/:id" children={<Item type="proveedores" loggedIn={loggedIn} showNotification={showNotification} />} />
                 <Route path="*">
                     <NoMatch />
                 </Route>
             </Switch>
-            <SignInModal show={signInShow} handleClose={handleSignInClose} setLoggedIn={setLoggedIn}/>
+            <SignInModal show={signInShow} handleClose={handleSignInClose} setLoggedIn={setLoggedIn} />
+            <div
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                }}
+            >
+                {notifications.map(item => item)}
+
+            </div>
         </div>
     );
 }
--- public/src/components/Item.tsx
+++ public/src/components/Item.tsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import "../resources/item.css";
 import axios from 'axios';
-import { useParams } from "react-router-dom";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { useParams, useHistory } from "react-router-dom";
+import { Container, Row, Col, Form, Button, Modal } from "react-bootstrap";
 import identifiers from "../types/Identifiers.type";
 import Barcode from 'react-barcode';
 
 type ItemProp = {
-    type: keyof typeof identifiers
+    type: keyof typeof identifiers,
+    loggedIn: boolean,
+    showNotification: ((message: string) => void)
 }
 
 function Item(props: ItemProp) {
     const identifier = identifiers[props.type];
-    let { id } = useParams();
-    let [item, setItem] = useState<any>(null);
-    let [backup, setBackup] = useState<any>(null);
-    let [editable, setEditable] = useState(false);
+    const { id } = useParams();
+    const [item, setItem] = useState<any>(null);
+    const [backup, setBackup] = useState<any>(null);
+    const [editable, setEditable] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
+    const [updateModal, setUpdateModal] = useState(false);
+    const [reason, setReason] = useState("");
+    const history = useHistory();
+
+    let form = null;
+    let visualInfo = null;
+    let buttons = null;
 
 
     useEffect(() => {
@@ -32,8 +42,28 @@ function Item(props: ItemProp) {
     }
 
     const saveEdition = () => {
+        axios.put(identifier.request + "/" + id, { ...item }).then(res => {
+            setItem(res.data);
+            props.showNotification("Guardado Exitosamente!");
+        }).catch(err => {
+            console.log(err);
+            setItem(backup);
+            props.showNotification("Error al guardar, porfavor intente mas tarde.");
+        })
+        setEditable(false);
+    }
 
-        // TODO: use axios to send to backEnd
+    const deleteItem = () => {
+        axios.delete(identifier.request + "/" + id).then(res => {
+            // TODO: agregar al reporte
+            setEditable(false);
+            props.showNotification("Eliminado Exitosamente!");
+            history.push("/materiales/" + props.type);
+        }).catch(err => {
+            console.log(err);
+            props.showNotification("Error al eliminar, porfavor intente mas tarde.");
+        })
+
     }
 
     const cancelEdition = () => {
@@ -41,19 +71,19 @@ function Item(props: ItemProp) {
         setEditable(false);
     }
 
-    const updateForm = () => {
-        return (e:React.ChangeEvent<HTMLInputElement>) => {
-            //e.target.value
+    const updateForm = (field: string) => {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            let itemCopy = { ...item };
+            itemCopy[field] = e.target.value;
+            setItem(itemCopy);
         }
-        
+
     }
-    let form = null;
-    let visualInfo = null;
     if (item) {
         form = identifier.type.cols.map((col, id) =>
             <Form.Group key={id} controlId={"form" + col.name} className="itemData">
                 <Form.Label className="font-weight-bold">{col.text}</Form.Label>
-                <Form.Control type="text" value={item[col.name]} plaintext={(!(col.readonly)! && !editable) || (col.readonly)} readOnly={(!(col.readonly)! && !editable) || (col.readonly)} />
+                <Form.Control type="text" value={item[col.name]} onChange={updateForm(col.name)} plaintext={(!(col.readonly)! && !editable) || (col.readonly)} readOnly={(!(col.readonly)! && !editable) || (col.readonly)} />
             </Form.Group>
         );
         visualInfo =
@@ -65,23 +95,41 @@ function Item(props: ItemProp) {
 
                         alt=""
                     />
-                    <Form.Control type="text" value={item.fotografia} plaintext={!editable} readOnly={!editable} style={{display:!editable?"none":"block"}}/>
+                    <Form.Control type="text" value={item.fotografia} onChange={updateForm("fotografia")} plaintext={!editable} readOnly={!editable} style={{ display: !editable ? "none" : "block" }} />
                 </div>
                 }
                 {"codigo_barras" in item && <div id="item-barcode">
                     <Barcode value={item.codigo_barras} />
-                    <Form.Control type="text" value={item.codigo_barras} plaintext={!editable} readOnly={!editable} style={{display:!editable?"none":"block"}} />
+                    <Form.Control type="text" value={item.codigo_barras} onChange={updateForm("codigo_barras")} plaintext={!editable} readOnly={!editable} style={{ display: !editable ? "none" : "block" }} />
                 </div>
                 }
 
                 {"descripcion" in item && <div id="item-descripcion">
                     <Form.Group controlId={"formDescripcion"} className="itemData">
                         <Form.Label className="font-weight-bold">Descripcion</Form.Label>
-                        <Form.Control type="text" value={item.descripcion} plaintext={!editable} readOnly={!editable} />
+                        <Form.Control as="textarea" rows="3" value={item.descripcion} onChange={updateForm("descripcion")} plaintext={!editable} readOnly={!editable} />
                     </Form.Group>
                 </div>
                 }
             </div>
+    }
+
+    if (props.loggedIn) {
+        if (!editable) {
+            buttons = <div>
+                <Button size="lg" variant="outline-primary" block onClick={startEdition}>Editar</Button>
+                {(item!= null && "cantidad" in item) &&
+                    <Button size="lg" variant="outline-info" block onClick={() => setUpdateModal(true)}>Modificar Cantidad</Button>
+                }
+            </div>
+        }
+        else {
+            buttons = <div>
+                <Button size="lg" variant="outline-success" block onClick={saveEdition}>Guardar</Button>
+                <Button size="lg" variant="outline-danger" block onClick={() => setDeleteModal(true)}>Eliminar</Button>
+                <Button size="lg" variant="outline-secondary" block onClick={cancelEdition}>Cancelar Cambios</Button>
+            </div>
+        }
     }
 
     return (
@@ -89,23 +137,60 @@ function Item(props: ItemProp) {
             <Row>
                 <Col>{visualInfo}</Col>
                 <Col><Form>{form}</Form></Col>
-                <Col>
-                    {!editable && <div>
-                        <Button size="lg" variant="outline-primary" block onClick={startEdition}>Editar</Button>
-                        <Button size="lg" variant="outline-info" block>Modificar Cantidad</Button>
-                    </div>
-                    }
-                    {editable && <div>
-                        <Button size="lg" variant="outline-success" block>Guardar</Button>
-                        <Button size="lg" variant="outline-danger" block>Eliminar</Button>
-                        <Button size="lg" variant="outline-secondary" block onClick={cancelEdition}>Cancelar Cambios</Button>
-                    </div>
-                    }
-
-                    {/* Edit Button -> (save, cancel, delete buttons) &  Edit quantity -> (opens modal)*/}
-                </Col>
+                <Col>{buttons}</Col>
             </Row>
-        </Container>
+            <Modal
+                show={deleteModal}
+                onHide={() => setDeleteModal(false)}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Eliminar
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId={"formDescripcion"} className="itemData">
+                        <Form.Label className="font-weight-bold">Explicacion</Form.Label>
+                        <Form.Control as="textarea" rows="3" value={reason} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setReason(e.target.value) }} />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    Seguro que desea borrar este elemento.
+                <Button variant="danger" onClick={deleteItem}>Eliminar</Button>
+                    <Button variant="secondary" onClick={() => { setReason(""); setDeleteModal(false); }}>Cancelar</Button>
+                </Modal.Footer>
+            </Modal>
+            {(item!= null && "cantidad" in item) &&
+                <Modal
+                    show={updateModal}
+                    onHide={() => setUpdateModal(false)}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Modificar cantidad
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form.Group controlId={"formDescripcion"} className="itemData">
+                            <Form.Label className="font-weight-bold">Nueva cantidad</Form.Label>
+                                <Form.Control type="number" value={item["cantidad"]} onChange={updateForm("cantidad")} min={0}/>
+                            <Form.Label className="font-weight-bold">Explicacion</Form.Label>
+                            <Form.Control as="textarea" rows="3" value={reason} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setReason(e.target.value) }} />
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" onClick={()=>{saveEdition(); setUpdateModal(false)}}>Modificar</Button>
+                        <Button variant="secondary" onClick={() => { setReason(""); setUpdateModal(false); cancelEdition();}}>Cancelar</Button>
+                    </Modal.Footer>
+                </Modal>
+            }
+        </Container >
     );
 }
 
